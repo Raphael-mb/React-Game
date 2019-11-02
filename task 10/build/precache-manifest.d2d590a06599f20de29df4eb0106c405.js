038f851f40bc51868f7aecc112428cd32c52976e
Raphael-mb <53257515+Raphael-mb@users.noreply.github.com>
2019-11-02 16:08:48 +0200

    Add files via upload

---
+++ task 10/build/precache-manifest.d2d590a06599f20de29df4eb0106c405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208ae743a822c4d0ef65f72a3f86798a",
+    "url": "/Raphael-mb.github.io/React-Game/index.html"
+  },
+  {
+    "revision": "0b6c9a9b2c36f458fd1c",
+    "url": "/Raphael-mb.github.io/React-Game/static/css/main.ef8640e2.chunk.css"
+  },
+  {
+    "revision": "66b8b03938ecabc49265",
+    "url": "/Raphael-mb.github.io/React-Game/static/js/2.66224a4f.chunk.js"
+  },
+  {
+    "revision": "0b6c9a9b2c36f458fd1c",
+    "url": "/Raphael-mb.github.io/React-Game/static/js/main.80b42f34.chunk.js"
+  },
+  {
+    "revision": "57b3318715e13674470c",
+    "url": "/Raphael-mb.github.io/React-Game/static/js/runtime-main.68571afe.js"
+  }
+]);
